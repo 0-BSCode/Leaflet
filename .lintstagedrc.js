f4a3a46d9517bb84988997f639a14aa1a5e9ace8
0-BSCode <bryanipsanchez@gmail.com>
2024-09-12 10:00:30 +0800

    Test lintstaged and husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    './**/src/**/*.{ts,tsx}': [
-        (files) => `eslint ${files.join(',')} --fix`,
+    './**/*.{ts,tsx}': [
+        (files) => `eslint ${files.join(' ')} --fix`,
     ],
-    './**/src/**/*.{ts,tsx,css,html,json}': [
-        (files) => `prettier ${files.join(',')} --write`,
+    './**/*.{ts,tsx,css,html,json}': [
+        (files) => `prettier ${files.join(' ')} --write`,
     ],
 };
