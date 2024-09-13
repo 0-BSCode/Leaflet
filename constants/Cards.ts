38ba48f978b01523f9422e6425f28e73aefd3bf8
0-BSCode <bryanipsanchez@gmail.com>
2024-09-13 10:51:05 +0800

    feat(1-dummy-data): added dummy data for cards
    TR-1

---
+++ constants/Cards.ts
@@ -0,0 +1,59 @@
+import { CardDto } from "@/dto/Card.dto";
+
+const Cards: CardDto = [
+  {
+    id: "1",
+    front: "What is 2 + 2?",
+    back: "4",
+  },
+  {
+    id: "2",
+    front: "What is 2 + 3?",
+    back: "5",
+  },
+  {
+    id: "3",
+    front: "What is 2 + 4?",
+    back: "6",
+  },
+  {
+    id: "4",
+    front: "What is 2 + 5?",
+    back: "7",
+  },
+  {
+    id: "5",
+    front: "What is 2 + 6?",
+    back: "8",
+  },
+  {
+    id: "6",
+    front: "What is 2 + 7?",
+    back: "9",
+  },
+  {
+    id: "7",
+    front: "What is 2 + 8?",
+    back: "10",
+  },
+  {
+    id: "8",
+    front: "What is 2 + 9?",
+    back: "11",
+  },
+  {
+    id: "9",
+    front: "What is 2 + 10?",
+    back: "12",
+  },
+  {
+    id: "10",
+    front: "What is 2 + 11?",
+    back: "13",
+  },
+  {
+    id: "11",
+    front: "What is 2 + 12?",
+    back: "14",
+  },
+];
