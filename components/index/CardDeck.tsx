fc753d43d52b7430b31f9e1f1ecbff1e1f486af1
0-BSCode <bryanipsanchez@gmail.com>
2024-09-15 01:03:24 +0800

    feat(tr-4): initial implementation of timer hook on cards

--- components/index/CardDeck.tsx
+++ components/index/CardDeck.tsx
@@ -1,34 +1,55 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, StyleSheet, Text } from "react-native";
 import { CardDto } from "@/dto/Card.dto";
 import { Card } from "./Card";
 import { Ionicons } from "@expo/vector-icons";
 import PagerView from "react-native-pager-view";
+import { useTimer } from "@/hooks/useTimer";
 
 interface CardDeckProps {
   deckData: CardDto[];
 }
 
 const PAGE_MARGIN = 20;
+const INITIAL_PAGE = 1;
+const AUTOPLAY_INTERVAL = 5000;
 
 export const CardDeck: React.FC<CardDeckProps> = ({ deckData }) => {
-  const pagerRef = React.createRef<PagerView>();
   const deckSize = deckData.length;
+  const [isPlaying, setIsPlaying] = React.useState<boolean>(false);
+  const [currentPage, setCurrentPage] = React.useState<number>(1);
+  const pagerRef = React.createRef<PagerView>();
+  const { pause, resume } = useTimer(
+    () => setCurrentPage((prev) => (prev + 1) % deckSize),
+    AUTOPLAY_INTERVAL,
+  );
 
   const handlePageChange = (position: number) => {
     if (position === 0) {
-      pagerRef.current?.setPage(deckData.length);
+      setCurrentPage(deckSize);
     } else if (position === deckSize + 1) {
-      pagerRef.current?.setPage(1);
+      setCurrentPage(1);
     }
   };
+
+  useEffect(() => {
+    pagerRef.current?.setPage(currentPage);
+  }, [currentPage]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      resume();
+    } else {
+      pause();
+    }
+  }, [isPlaying]);
 
   return (
     <View style={styles.mainContainer}>
       <View style={styles.progressBar}></View>
       <View style={styles.setContainer}>
         <PagerView
-          initialPage={1}
+          initialPage={INITIAL_PAGE}
           ref={pagerRef}
           onPageSelected={(e) => handlePageChange(e.nativeEvent.position)}
           style={styles.setContainer}
@@ -42,10 +63,33 @@ export const CardDeck: React.FC<CardDeckProps> = ({ deckData }) => {
           <Card data={deckData[0]} key={"first-clone"} />
         </PagerView>
       </View>
+      <Text>{currentPage}</Text>
 
       <View style={styles.controlsContainer}>
         <View style={styles.button}>
-          <Ionicons name="play" size={30} color="black" />
+          {isPlaying ? (
+            <Ionicons
+              name="pause"
+              size={30}
+              color="black"
+              // onPress={() => setIsPlaying(false)}
+              onPress={() => {
+                pause();
+                setIsPlaying(false);
+              }}
+            />
+          ) : (
+            <Ionicons
+              name="play"
+              size={30}
+              color="black"
+              // onPress={() => setIsPlaying(true)}
+              onPress={() => {
+                resume();
+                setIsPlaying(true);
+              }}
+            />
+          )}
         </View>
         <View style={styles.button}>
           <Ionicons name="shuffle" size={30} color="black" />
