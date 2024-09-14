6b788dcc0d6d5533852f667f8e1fd12edddbe17b
0-BSCode <bryanipsanchez@gmail.com>
2024-09-15 00:46:58 +0800

    refactor(tr-4): remove unused code for timer hook

--- hooks/useTimer.ts
+++ hooks/useTimer.ts
@@ -2,15 +2,12 @@ import React from "react";
 
 export function useTimer(callback: () => void, delay: number) {
   const [timerId, setTimerId] = React.useState<NodeJS.Timeout | null>(null);
-  const [start, setStart] = React.useState<number>(Date.now());
-  const [remaining, setRemaining] = React.useState<number>(delay);
 
   function resume() {
     if (timerId) {
       return;
     }
 
-    setStart(Date.now());
     setTimerId(setInterval(callback, delay));
   }
 
@@ -18,7 +15,6 @@ export function useTimer(callback: () => void, delay: number) {
     if (timerId !== null) {
       clearInterval(timerId);
       setTimerId(null);
-      setRemaining(remaining - (Date.now() - start));
     }
   }
 
